9283a1ba2da8e849e1b277602ed1611ac7f5abd1
facundo roman <facundoagustinroman01@gmail.com>
2026-03-16 18:59:52 -0300

    cambio

--- src/components/UseScrollToTop.jsx
+++ src/components/UseScrollToTop.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
-const useScrollToTop = () => {
+const UseScrollToTop = () => {
   // const { pathname } = useLocation();
 
   // useEffect(() => {
@@ -21,4 +21,4 @@ const useScrollToTop = () => {
   return null;
 };
 
-export default useScrollToTop;
+export default UseScrollToTop;
